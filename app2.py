78b1e6a886052b538a9ad87475288a200cb3834f
Luana Barros-LUANAFB <luanafb@motorola.com>
2022-12-16 17:26:21 -0300

    Usando modelo pt-en

--- app2.py
+++ app2.py
@@ -1,12 +1,14 @@
 from flask import Flask, request, render_template
-# from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
-from transformers import M2M100ForConditionalGeneration, M2M100Tokenizer
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
+# from transformers import M2M100ForConditionalGeneration, M2M100Tokenizer
 import socket
   
-# translate Chinese to English
-tokenizer = M2M100Tokenizer.from_pretrained("facebook/m2m100_418M")
-model = M2M100ForConditionalGeneration.from_pretrained("facebook/m2m100_418M")
-tokenizer.src_lang = "en"
+tokenizer = AutoTokenizer.from_pretrained("unicamp-dl/translation-pt-en-t5")
+
+model = AutoModelForSeq2SeqLM.from_pretrained("unicamp-dl/translation-pt-en-t5")
+
+# pten_pipeline = pipeline('text2text-generation', model=model, tokenizer=tokenizer)
+
 # instanciate Flask
 app = Flask(__name__)
 
@@ -17,9 +19,11 @@ def index():
         return render_template("index.html", hostname = hostname)
 
     input = request.get_data().decode('UTF-8')
-    encoded_zh = tokenizer(input, return_tensors="pt")
-    generated_tokens = model.generate(**encoded_zh, forced_bos_token_id=tokenizer.get_lang_id("pt"))
+    encoded_zh = tokenizer("translate Portuguese to English: " + input, return_tensors="pt")
+    generated_tokens = model.generate(**encoded_zh)
+    
     translated_text = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
+    # translated_text = pten_pipeline("translate Portuguese to English: " + input)[0]['generated_text']
     return {'translated_text': translated_text}
 
 if __name__ == '__main__':
